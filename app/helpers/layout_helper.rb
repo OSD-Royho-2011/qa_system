1ed7d475a9f14afc4cf0b8f3b12787203cc70c12
UiCA <ui.coor@orientsoftware.com>
2021-11-02 17:59:50 +0700

    Authentication: Apply Sigin/Signup form

---
+++ app/helpers/layout_helper.rb
@@ -0,0 +1,10 @@
+
+module LayoutHelper
+    
+    # Used to achieve nested layouts without content_for
+    def parent_layout(layout)
+        @view_flow.set(:layout, output_buffer)
+        output = render(:template => "#{layout}")
+        self.output_buffer = ActionView::OutputBuffer.new(output)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "auth#signin"
+
+  get "/signin", to: "auth#signin"
+  get "/signup", to: "auth#signup"
 end
